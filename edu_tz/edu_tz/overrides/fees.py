3518a4e4cac25468244f2e753471622e5c323f06
av-dev2 <emgani@aakvatech.com>
2026-08-20 15:38:29 +0300

    feat: own Fees abbreviation and cancellation hooks

---
+++ edu_tz/edu_tz/overrides/fees.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+import frappe
+
+
+@frappe.whitelist()
+def set_fee_abbr(doc: Any = None, method: Any = None):
+    doc.company = frappe.get_value("Fee Structure", doc.fee_structure, "company")
+    if not frappe.get_value("Company", doc.company, "send_fee_details_to_bank"):
+        return
+    doc.abbr = frappe.get_value("Company", doc.company, "abbr")
+
+
+def on_cancel_fees(doc: Any, method: Any):
+    from erpnext.accounts.utils import unlink_ref_doc_from_payment_entries
+
+    from edu_tz.edu_tz.nmb.api import cancel_invoice
+
+    unlink_ref_doc_from_payment_entries(doc)
+    cancel_invoice(doc, "before_cancel")
