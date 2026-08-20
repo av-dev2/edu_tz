dec1f3f8d1a069ca971da07ca6ea9d993296e068
av-dev2 <emgani@aakvatech.com>
2026-08-20 15:38:34 +0300

    feat: own the NMB Callback controller

---
+++ edu_tz/edu_tz/doctype/nmb_callback/nmb_callback.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2020, Aakvatech and contributors
+# For license information, please see license.txt
+
+
+# import frappe
+from frappe.model.document import Document
+
+
+class NMBCallback(Document):
+    pass
