681f20ece0c700d3084e6123b1c0f183676e8147
av-dev2 <emgani@aakvatech.com>
2026-08-20 15:38:59 +0300

    feat: own the Company fee account filters

---
+++ edu_tz/edu_tz/company.js
@@ -0,0 +1,22 @@
+frappe.ui.form.on("Company", {
+	setup: function (frm) {
+		frm.set_query("fee_bank_account", function () {
+			return {
+				"filters": {
+					"company": frm.doc.name,
+					"account_type": ["in", ["Cash", "Bank"]],
+					"account_currency": frm.doc.default_currency,
+				}
+			};
+		});
+		frm.set_query("student_applicant_fees_revenue_account", function () {
+			return {
+				"filters": {
+					"company": frm.doc.name,
+					"account_type": "Income Account",
+					"account_currency": frm.doc.default_currency,
+				}
+			};
+		});
+	},
+});
