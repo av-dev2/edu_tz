423931efb6bc4753201210d6f5bbee5f0be7cf89
av-dev2 <emgani@aakvatech.com>
2026-08-27 15:42:21 +0300

    test: move Edu Tz Settings onto IntegrationTestCase

--- edu_tz/edu_tz/doctype/edu_tz_settings/test_edu_tz_settings.py
+++ edu_tz/edu_tz/doctype/edu_tz_settings/test_edu_tz_settings.py
@@ -2,12 +2,12 @@
 # See license.txt
 
 import frappe
-from frappe.tests.utils import FrappeTestCase
+from frappe.tests import IntegrationTestCase
 
 # Company records come from ERPNext's test setup, not from this app.
-test_ignore = ["Company"]
+IGNORE_TEST_RECORD_DEPENDENCIES = ["Company"]
 
 
-class TestEduTzSettings(FrappeTestCase):
+class TestEduTzSettings(IntegrationTestCase):
 	def test_partial_payment_defaults_off(self):
 		self.assertFalse(frappe.db.get_single_value("Edu Tz Settings", "partial_payment"))
